1f046f6d3f6c04025e80e33e374d45a7505fc603
zhuikov <artem.zhuikov@mail.ru>
2019-11-12 00:20:52 +0300

    task1_2

--- Task1_2.hs
+++ Task1_2.hs
@@ -13,15 +13,24 @@ sin x = todo
 
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
-cos x = todo
+cos x = logic x 10 0 where
+    logic _ (-1) sum = sum
+    logic x n sum = logic x (n - 1) ( sum + (-1)**n * x**(2 * n) / product [1..2 * n] ) 
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y = logic (abs x) (abs y) where
+    logic x 0 = x
+    logic 0 y = y
+    logic x y | x > y  = logic y (x `mod` y)
+              | x < y  = logic x (y `mod` x)
+              | x == y = x
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
+doesSquareBetweenExist from to | from > to = False
+                               | otherwise = not $ null squaresBetweenFromTo where
+    squaresBetweenFromTo = filter (>= from) $ takeWhile (< to) $ map (^2) [0..]
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
@@ -35,7 +44,10 @@ pow x y = todo
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x = logic $ abs x where
+    logic 1 = False
+    logic x | even x    = x == 2
+            | otherwise = null [ num | num <- [3,5..x - 1], x `mod` num == 0 ]
 
 type Point2D = (Double, Double)
 
