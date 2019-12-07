22a62e3c4e45cfb34aa6110ce0bc6ecb105e0eaa
zhuikov <artem.zhuikov@mail.ru>
2019-12-07 15:57:03 +0300

    task 4_1

--- Task4_1.hs
+++ Task4_1.hs
@@ -11,3 +11,64 @@ module Task4_1 where
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Functor FunMonad where
+
+-- Применение функции f к результату фукнции fun, лежащей в контексте монады.
+-- Создание контекста с новым поведением.
+-- Functor f => (a -> b) -> f a -> f b
+
+    fmap f (FunMonad fun) = FunMonad (f . fun)
+
+{-
+    a = FunMonad length
+    f = (+10)
+    fun (fmap f a) "abc" === 13
+-}
+
+-- Закон fmap id x === x выполнен
+
+instance Applicative FunMonad where
+    
+-- Обернуть в FunMonad переданную функцию.
+
+    pure f = FunMonad (\x -> f)
+
+-- Applicative f => f (a -> b) -> f a -> f b
+-- Заменяя, a на (String -> a) получим:
+-- Applicative f => f (String -> a -> b) -> f (String -> a) -> f b
+-- Таким образом, fun1 принимает два аргумента: String и a, возвращая b.
+
+    (<*>) (FunMonad fun1) (FunMonad fun2) = FunMonad (\x -> fun1 x $ fun2 x )
+
+{- 
+    Пример.
+
+    a :: Integral a => t -> a -> Bool
+    a str len = even $ len
+
+    b :: Foldable t => t a -> Int
+    b = length
+
+    a' = FunMonad a
+    b' = FunMonad b
+    c  = a' <*> b'
+
+    fun c "123" === False
+-}
+
+
+instance Monad FunMonad where
+    
+    return = pure
+
+-- Monad m => m a -> (a -> m b) -> m b
+-- FunMonad (String -> a) -> (a -> FunMonad (String -> b) -> FunMonad (String -> b)
+-- fun1 x - применили функцию из монады к аргументу - получили тип a
+-- f $ fun1 x - получили FunMonad (String -> b)
+-- fun (f $ fun1 x) - "достали" из полученной монады функцию
+-- fun (f $ fun1 x) x - применили эту функцию к аргументу.
+-- Получили неоходимую монаду
+
+    (>>=) (FunMonad fun1) f = FunMonad (\x -> fun (f $ fun1 x) x )
+
