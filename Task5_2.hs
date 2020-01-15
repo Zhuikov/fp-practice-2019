11c16c7041325c05ba5671b4b7667e5120d626db
zhuikov <artem.zhuikov@mail.ru>
2020-01-15 19:22:32 +0300

    task 5_2

--- Task5_2.hs
+++ Task5_2.hs
@@ -1,6 +1,7 @@
 module Task5_2 where
 
 import Todo(todo)
+import Prelude hiding (concat)
 
 -- Зиппер из лекции 
 
@@ -31,14 +32,61 @@ removeRight (Zipper l (_:rt)) = Zipper l rt
 removeLeft :: Zipper a -> Zipper a
 removeLeft (Zipper (_:lt) r) = Zipper lt r
 
+instance (Show a) => Show (Zipper a)  where
+    show z = show r
+        where (Zipper l r) = toLeftEnd z
+
+instance (Eq a) => Eq (Zipper a) where
+
+    (==) z1 z2 = r1 == r2
+        where (Zipper _ r1) = toLeftEnd z1
+              (Zipper _ r2) = toLeftEnd z2
+
+-- Выводит внутреннее состояние зиппера
+showParts :: (Show a) => Zipper a -> String
+showParts (Zipper l r) = show l ++ " " ++ show r
+
 -- Используя приведённые выше функции, реализуйте функцию конкатенации
 -- вставки подсписка в середину и выделения подсписка
 
+-- Функция перемещения указателя зиппера в левый конец
+-- Правый список в зиппере в таком случае соотвествует первоначальному списку (см. fromList)
+toLeftEnd :: Zipper a -> Zipper a
+toLeftEnd z@(Zipper l r) = logic z $ length l
+    where logic z 0 = z
+          logic z n = logic (goLeft z) $ n - 1
+
 concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concat left@(Zipper l1 r1) right = Zipper l1 (r1 ++ r2)
+    where (Zipper _ r2) = toLeftEnd right
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
+insertManyAt index what@(Zipper left right) to@(Zipper l r)
+    | index > length l  = insertManyAt index what $ goRight to
+    | index < length l  = insertManyAt index what $ goLeft to
+    | index == length l = Zipper (left ++ l) (right ++ r)
 
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper from to input@(Zipper l r) 
+    | from > length l  = subZipper from to $ goRight input
+    | from < length l  = subZipper from to $ goLeft input
+    | from == length l = fromList $ drop 0 . take (to - from) $ r
+
+{-
+    a = fromList [1,2,3,4,5]
+    b = fromList [10,11,12,13]
+
+    c = concat a b
+    c === [1,2,3,4,5,10,11,12,13]
+
+    ins = fromList [(-1), (-2), (-3), (-4), (-5)]
+    ins1 = goRight ins
+    ins2 = goRight ins1
+
+    ins2 === [(-1), (-2), (-3), (-4), (-5)]
+    showParts ins2 === "[-2,-1] [-3,-4,-5]"
+
+    res = insertManyAt 4 ins2 c
+    res === [1,2,3,4,-1,-2,-3,-4,-5,5,10,11,12,13]
+    showParts res == "[-2,-1,4,3,2,1] [-3,-4,-5,5,10,11,12,13]"
+-}
